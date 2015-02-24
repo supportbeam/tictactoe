c49dfdd8860c5eb3ef41042abffa93e836467405
JJLim <lim.jess9@gmail.com>
2015-02-23 23:18:25 -0500

    Added functions to track winning

--- scripts.js
+++ scripts.js
@@ -8,9 +8,10 @@ $(document).ready(function() {
     player = determinePlayer(turn) // player is either X or O
     self.addClass(player) //add a class X or O to the td self
     self.text(player) // add the player X or O in the td
-    console.log(positionsPlayed(player))
+    positions = positionsPlayed(player)
+    console.log(positions) //output the positions
 
-    // checkwin();
+    checkwin(positions);
     // console.log(turn);
     turn++;
     self.off('click');
@@ -30,21 +31,36 @@ $(document).ready(function() {
   //   };
   // };
 
-  
+
+  //returns an array of positions played by the player
   function positionsPlayed(player){
     return $('.' + player).map(function(idx, node){
       return parseInt($(node).attr('id'));
     }).get();
   }
 
-  function match(x, y, z) {
-    return (($(x).text() === $(y).text()) && ($(y).text() === $(z).text()))
+  // function match(x, y, z) {
+  //   return (($(x).text() === $(y).text()) && ($(y).text() === $(z).text()))
+  // };
+
+  function checkwin(positions) {
+    winners = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [5, 7, 9]]
+    //for each combo in winners
+    if (positions.length >= 3) {
+      for (i=0; i <= winners.length; i++) {
+        var combo = winners[i];
+        for (j=0; j <= positions; j++) {
+          return combo.include(positions[j]);
+          console.log("You Win!");
+        }
+      }
+      // check if the positions >= 3 and includes all the #s from any of the winning combos
+      //return true or false for win
+    }
   };
 
 });
 
 // $('td').text.remove()
-
-winners = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [5, 7, 9]]
 
 // for combo in winners, pass it in match and check if it wins after each click
